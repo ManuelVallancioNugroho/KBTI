a5092ac7ed49bd5c7a6ca27fbe8e1d54290a8a3d
ManuelVallancioNugroho <manuel.vallancio@si.ukdw.ac.id>
2022-06-12 13:28:18 +0700

    fix home

--- home.php
+++ home.php
@@ -2,5 +2,4 @@
 <img src="assets/images/logo.png" width="200px height="200px" /> <br>
 <font Size="6" face="Helvetica">TEMALI</font> <br>
 <font Size="6" face="Helvetica">SISTEM INFORMASI PERWALIAN</font> <br>
-<font Size="6">UNIVERSITAS KRISTEN DUTA WACANA</font>
 </center>
