aabe80fb08c7fa04ab6b238018049225dc14dbda
ManuelVallancioNugroho <manuel.vallancio@si.ukdw.ac.id>
2022-06-12 13:12:41 +0700

    ubah logo

--- home.php
+++ home.php
@@ -1,5 +1,5 @@
 <center><br><br><br><br><br><br><br><br>
-<img src="assets/images/33.UKDW.png" width="200px height="200px" /> <br>
+<img src="assets/images/logo.png" width="200px height="200px" /> <br>
 <font Size="6" face="Helvetica">TEMALI</font> <br>
 <font Size="6" face="Helvetica">SISTEM INFORMASI PERWALIAN</font> <br>
 <font Size="6">UNIVERSITAS KRISTEN DUTA WACANA</font>
--- perwalian.php
+++ perwalian.php
@@ -2,7 +2,7 @@
 <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-<img src="assets/images/33.UKDW.png" width="50px height="50px" />  
+<img src="assets/images/logo.png" width="50px height="50px" />  
 <a class="navbar-brand" href="index.php">TEMALI</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
